f3026bde482981515db984870ae67c48efc6aabb
Serpens10 <147108197+Serpens10@users.noreply.github.com>
2023-11-15 15:38:07 +0100

    draw player

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
 from settings import TILESIZE
+from player import Player
 
 class Level :
     def __init__(self, level_data, surface):
@@ -12,16 +13,24 @@ class Level :
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
             
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
-                if cell == 'X' :
-                    x = col_index * TILESIZE
-                    y = row_index * TILESIZE
+                 x = col_index * TILESIZE
+                 y = row_index * TILESIZE
+
+                 if cell == 'X' :
                     tile = Tile((x, y), TILESIZE)
                     self.tiles.add(tile)
+                 if cell == 'P' :  
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
 
 
     def run(self):
+        # level tiles
         self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)   
+        self.tiles.draw(self.display_surface)  
+        # player 
+        self.player.draw(self.display_surface)
--- tiles.py
+++ tiles.py
@@ -1,12 +1,12 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, position, size):
+    def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
         # size: x and y. identical because square.
         self.image.fill('grey')
-        self.rect = self.image.get_rect(topleft = position)
+        self.rect = self.image.get_rect(topleft = pos)
 
     def update(self, x_shift): 
         self.rect.x += x_shift   
